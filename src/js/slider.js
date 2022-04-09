b7816aeced4f73ea2db0b675c61ad748033e1244
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2022-04-09 18:25:16 +0200

    added js slider

---
+++ src/js/slider.js
@@ -0,0 +1,27 @@
+var slides  = document.querySelectorAll('.container-test > .container-project_content');
+var prev = document.querySelector('.icon-arrow-left');
+var next = document.querySelector('.icon-arrow-right')
+
+var currentSlide = 0;
+
+function nextSlide() {
+  changeSlide(currentSlide+1);
+}
+function prevSlide(){
+  changeSlide(currentSlide-1);
+}
+
+function changeSlide(n){
+  slides[currentSlide].className = 'hide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'container-project_content show';
+}
+
+next.onclick = function(){
+  nextSlide();
+}
+prev.onclick = function(){
+  prevSlide();
+}
+
+
